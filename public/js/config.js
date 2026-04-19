e92b36b2d1dcd6a4cfdbe7b90b6cf7773477ea74
Seu Nome <seu@email.com>
2026-04-19 13:17:31 -0600

    fix: add Supabase publishable key

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,2 @@
 export const SUPABASE_URL = 'https://ncausitrddvtoyivolkj.supabase.co';
-export const SUPABASE_ANON_KEY = '<YOUR_SUPABASE_ANON_KEY>';
+export const SUPABASE_ANON_KEY = 'sb_publishable_l6DUrgk9d6JFrCLTuxn16Q_lO5qdHY2';
