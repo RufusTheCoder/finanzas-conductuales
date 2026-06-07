8c1894924067dd1fb356f083b613500a58007d67
Seu Nome <seu@email.com>
2026-06-07 00:56:41 -0600

    chore(i18n): bump cache version to 20260606b for Fase 1b checkpoint 1

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-import { signIn, signUp, createUserProfile, loadProgress, saveProgress, logResponses, logQuestionFeedback, logContentFeedback, createSession, updateSession, signInWithGoogle, signInWithFacebook, signInWithApple, getUser, setSession, requestPasswordReset, updatePassword, resendConfirmation, markOnboardingSeen, getUserProfile, saveNextSteps, getMyNextSteps, getNextStepsCounts, submitBug, setErrorContext, setReadOnly, refreshSession, fetchTranslations } from './supabase.js?v=20260606a';
+import { signIn, signUp, createUserProfile, loadProgress, saveProgress, logResponses, logQuestionFeedback, logContentFeedback, createSession, updateSession, signInWithGoogle, signInWithFacebook, signInWithApple, getUser, setSession, requestPasswordReset, updatePassword, resendConfirmation, markOnboardingSeen, getUserProfile, saveNextSteps, getMyNextSteps, getNextStepsCounts, submitBug, setErrorContext, setReadOnly, refreshSession, fetchTranslations } from './supabase.js?v=20260606b';
 import { SUPABASE_URL as _SBU, SUPABASE_ANON_KEY as _SBK } from './config.js';
 import { questions } from '../data/questions.js';
-import { SESGOS } from '../data/sesgos.js?v=20260606a';
+import { SESGOS } from '../data/sesgos.js?v=20260606b';
 import { BIT_PROFILES, bitLabel } from '../data/profiles.js';
-import { t, setLang, getLang, ingest } from './i18n.js';
+import { t, setLang, getLang, ingest } from './i18n.js?v=20260606b';
 
 const app = document.getElementById('app');
 
